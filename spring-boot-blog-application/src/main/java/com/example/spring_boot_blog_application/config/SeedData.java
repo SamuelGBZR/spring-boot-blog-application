87d8f2fdc56e05e03f885b635deaeb5483060e4f
Samuel <1155108842@link.cuhk.edu.hk>
2024-10-07 14:23:42 +0800

    Setup Account

--- spring-boot-blog-application/src/main/java/com/example/spring_boot_blog_application/config/SeedData.java
+++ spring-boot-blog-application/src/main/java/com/example/spring_boot_blog_application/config/SeedData.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.example.spring_boot_blog_application.models.Account;
 import com.example.spring_boot_blog_application.models.Post;
+import com.example.spring_boot_blog_application.services.AccountService;
 import com.example.spring_boot_blog_application.services.PostService;
 
 @Component
@@ -15,19 +17,40 @@ public class SeedData implements CommandLineRunner {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private AccountService accountService;
+
     @Override
     public void run(String... args) throws Exception {
         List<Post> posts = postService.getAll();
 
         if (posts.size() == 0) {
+
+            Account account1 = new Account();
+            account1.setFirstName("user");
+            account1.setLastName("user");
+            account1.setEmail("user.user@domain.com");
+            account1.setPassword("password");
+
+            Account account2 = new Account();
+            account2.setFirstName("admin");
+            account2.setLastName("admin");
+            account2.setEmail("admin.admin@domain.com");
+            account2.setPassword("password");
+
             Post post1 = new Post();
             post1.setTitle("Title of post 1");
             post1.setBody("This is the body of post 1");
-            postService.save(post1);
+            post1.setAccount(account1);
 
             Post post2 = new Post();
             post2.setTitle("Title of post 2");
             post2.setBody("This is the body of post 2");
+            post2.setAccount(account2);
+
+            accountService.save(account1);
+            accountService.save(account2);
+            postService.save(post1);
             postService.save(post2);
         }
     }
---
+++ spring-boot-blog-application/src/main/java/com/example/spring_boot_blog_application/models/Account.java
@@ -0,0 +1,30 @@
+package com.example.spring_boot_blog_application.models;
+
+import java.util.*;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Account {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+
+    @OneToMany(mappedBy = "account")
+    private List<Post> posts;
+}
