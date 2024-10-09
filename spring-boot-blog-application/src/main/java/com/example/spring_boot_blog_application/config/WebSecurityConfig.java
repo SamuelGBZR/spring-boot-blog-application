a5f2df5d8a4b87af756be5be961aca6a6de0dcec
Samuel <1155108842@link.cuhk.edu.hk>
2024-10-09 17:03:13 +0800

    Set security

---
+++ spring-boot-blog-application/src/main/java/com/example/spring_boot_blog_application/config/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.spring_boot_blog_application.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class WebSecurityConfig {
+    
+    private static final String[] WHITELIST = { "/register", "/login", "/posts/*", "/h2-console/*", "/" };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        
+        http.authorizeHttpRequests(
+                auth -> auth.requestMatchers(WHITELIST).permitAll().anyRequest().authenticated());
+        http.csrf(csrf -> csrf.disable());
+        http.headers(headers -> headers.frameOptions(frame -> frame.disable()));
+        return http.build();
+    }
+}
