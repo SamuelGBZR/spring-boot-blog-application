696f3b85110d6a8bf21d0a54f512f84bed2d526e
Samuel <1155108842@link.cuhk.edu.hk>
2024-10-14 17:02:50 +0800

    Add function for editing posts

--- spring-boot-blog-application/src/main/java/com/example/spring_boot_blog_application/controllers/PostController.java
+++ spring-boot-blog-application/src/main/java/com/example/spring_boot_blog_application/controllers/PostController.java
@@ -3,12 +3,11 @@ package com.example.spring_boot_blog_application.controllers;
 import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.spring_boot_blog_application.models.Account;
 import com.example.spring_boot_blog_application.models.Post;
@@ -54,6 +53,39 @@ public class PostController {
     @PostMapping("/posts/new")
     public String saveNewPost(@ModelAttribute Post post) {
         postService.save(post);
+        return "redirect:/posts/" + post.getId();
+    }
+
+    @GetMapping("/posts/{id}/edit")
+    @PreAuthorize("isAuthenticated()")
+    public String getPostforEdit(@PathVariable Long id, Model model) {
+
+        //  find post by id
+        Optional<Post> optionalPost = postService.getById(id);
+        //  if post exists, shove into model
+        if (optionalPost.isPresent()) {
+            Post post = optionalPost.get();
+            model.addAttribute("post", post);
+            return "post_edit";
+        } else {
+            return "404";
+        }
+    }
+    
+    @PostMapping("/posts/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public String updatePost(@PathVariable Long id, Post post, BindingResult result, Model model) {
+        
+        Optional<Post> optionalPost = postService.getById(id);
+        if (optionalPost.isPresent()) {
+            Post existingpost = optionalPost.get();
+            
+            existingpost.setTitle(post.getTitle());
+            existingpost.setBody(post.getBody());
+
+            postService.save(existingpost);
+        }
+
         return "redirect:/posts/" + post.getId();
     }
 }
